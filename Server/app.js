3a38f5d06219da65d240f62fa90870397d29781c
GodofKim <kjh.original@gmail.com>
2016-11-12 22:43:14 +0900

    Add : cars module
    다른 도메인에서 접근 가능하도록 처리함. 임시.

--- Server/app.js
+++ Server/app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var cors = require('cors');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -17,6 +17,7 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -56,8 +57,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(process.env.PORT||3000, function(){
-  console.log ("Server is running at 3000");
+app.listen(process.env.PORT||4000, function(){
+  console.log ("Server is running at 4000");
 });
 
 module.exports = app;
