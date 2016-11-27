bc4da63b2f54e5ffb22a388f4d9bb1004a727f82
Godof Kim <kjh.original@gmail.com>
2016-11-27 15:41:22 +0900

    Modify : attach textRank to fetched article
    윈도우에서는 형태소 분석기가 작동 안 하는듯.

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -67,9 +67,59 @@ router.get('/', function(req, res, next) {
 
 router.get('/hello', function (req, res, next) {
   read(req.headers.pageurl, function(err, article, meta) {
-    console.log("What is the type of article.content : ", typeof(article.content));
-    console.log("Content : ", article.content);
-    return res.json({ shorten: [req.headers.pageurl, "this is", "Awesome"] });
+
+    var text = "";
+    if(req.headers.pageurl.includes("news.naver")){
+      var dom = article.document;
+      text = dom.getElementById("articleBodyContents").textContent;
+    }
+    else{
+      text = article.textBody;
+    }
+    var sentences = ranker.splitToSentences(text);
+    var graph = [];
+    // 1st para in async.each() is the array of items
+    async.each(sentences,
+      // 2nd param is the function that each item is passed to
+      function(sentence, callback){
+        // Call an asynchronous function, often a save() to DB
+        var sentenceSimilarity = [];
+
+        async.each(sentences, function(sentence_2, callback_2) {
+          mecab.nouns(sentence, function(err, result) {
+            mecab.nouns(sentence_2, function(err, result_2) {
+              console.log("RESULT : ", result);
+              console.log("RESULT2 : ", result_2);
+              var index = jaccard.index(result, result_2);
+              sentenceSimilarity.push(index);
+              callback_2();
+            });
+          });
+        },
+        function(err) {
+          graph.push(sentenceSimilarity);
+          callback();
+        });
+      },
+      // 3rd param is the function to call when everything's done
+      function(err){
+        // 그래프를 얻었으니 텍스트랭크를 돌린다.
+        var Rank = ranker.getTextRank(graph).probabilityNodes;
+        // 가장 영향력이 큰 노드 세 개를 취한다.
+        var selectedIndex = ranker.getSelectedIndex(Rank, 3);
+        // 문장의 순서대로 정렬하는 게 문맥상 자연스러운 듯.
+        selectedIndex.sort();
+        var result = [];
+        for(var i = 0; i < 3; i++){
+          for(var j = 0; j < sentences.length; j++){
+            if(selectedIndex[i] === j){
+              result[i] = sentences[j];
+            }
+          }
+        }
+        return res.json({ shorten : result });
+      }
+    );
   });
 });
 
